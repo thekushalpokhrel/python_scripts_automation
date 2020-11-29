47fdee12b258b6a971166057e69f5da16a0900b7
kushal pokhrel <71119638+thekushalpokhrel@users.noreply.github.com>
2020-11-29 19:29:46 +1100

    Add files via upload

---
+++ date_checks_latest_return_date.py
@@ -0,0 +1,22 @@
+#he file_date function creates a new file in the current working directory,
+#checks the date that the file was modified,
+#and returns just the date portion of the timestamp in the format of yyyy-mm-dd.
+#Fill in the gaps to create a file called "newfile.txt" and check the date that it was modified.
+
+import os
+import datetime
+
+def file_date(filename):
+  # Create the file in the current directory
+  with open (filename,'w') as file:
+    pass
+  timestamp = os.path.getmtime(filename)
+  c=datetime.datetime.fromtimestamp(timestamp)
+  # Convert the timestamp into a readable format, then into a string
+
+  # Return just the date portion
+  # Hint: how many characters are in “yyyy-mm-dd”?
+  return ("{}".format(c.strftime("%Y-%m-%d")))
+
+print(file_date("newfile.txt"))
+# Should be today's date in the format of yyyy-mm-dd
