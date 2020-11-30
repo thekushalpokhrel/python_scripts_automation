b2c60d7a6907bbf3899945f510df16f2b90156e2
kushal pokhrel <71119638+thekushalpokhrel@users.noreply.github.com>
2020-12-01 06:56:18 +1100

    Update createDir_renameDir_addnewDir.py

--- createDir_renameDir_addnewDir.py
+++ createDir_renameDir_addnewDir.py
@@ -1,13 +1,13 @@
 #The create_python_script function creates a new python script in the current working-
 #-directory, adds the line of comments to it declared by the 'comments' variable,
 #and returns the size of the new file to create a script called "program.py".
+import os
 
 def create_python_script(filename):
-  comments = "# Start of a new Python program"
-  with open (filename, "w") as file:
-    file.write(comments)
-    import os
+    comments = "# Start of a new Python program"
+    with open(filename, "w") as f:
+        f.write(comments)
     filesize = os.path.getsize(filename)
-  return(filesize)
+    return(filesize)
 
 print(create_python_script("program.py"))
