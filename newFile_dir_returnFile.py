48667d9888aa8ae03a1f5ca612a4af71692d0378
kushal pokhrel <71119638+thekushalpokhrel@users.noreply.github.com>
2020-12-01 06:57:45 +1100

    Update newFile_dir_returnFile.py

--- newFile_dir_returnFile.py
+++ newFile_dir_returnFile.py
@@ -1,11 +1,12 @@
 #The new_directory function creates a new directory inside the current working directory,
-then creates a new empty file inside the new directory, and returns the list of files in that directory.
+#then creates a new empty file inside the new directory, and returns the list of files in that directory.
 
 import os
+
 def new_directory(directory, filename):
   # Before creating a new directory, check to see if it already exists
   if os.path.isdir(directory) == False:
-    os.mkdir(directory)
+    os.makedirs(directory, exist_ok=True)
 
   # Create the new file inside of the new directory
   os.chdir(directory)
